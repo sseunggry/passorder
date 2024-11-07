7b2d54812fa96e0fa9a009083fe2399fdb159f8b
sseung <dptmejf34@gmail.com>
2024-11-07 23:04:04 +0900

    glitch 사용해서 json 데이터 받아오기

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,7 @@
 // const FOLDER_NAME = "passorder";
 // const BASE_PATH = `https://sseunggry.github.io/dataApi/data/${FOLDER_NAME}`;
-const BASE_PATH = `http://localhost:3001`;
+// const BASE_PATH = `http://localhost:3001`;
+const BASE_PATH = `https://ambitious-periwinkle-monarch.glitch.me`;
 
 // export const fetchData = (name: string) => {
 //     return fetch(`${BASE_PATH}/${name}.json`).then((response) => response.json());
